63a8c84ec08dadc1f3a5e8f92c5ee7046e441410
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2010-12-15 23:04:03 +0100

    ListUtil - cos, co nam sie przyda!!!

---
+++ TO_1/TO_1/ListUtil.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TO_1
+{
+    public static class ListUtil
+    {
+        public static int Distance(this IList<Point> first, IList<Point> second)
+        {
+            return CalculateCenterOfMass(first).Distance(CalculateCenterOfMass(second));
+        }
+
+        private static Point CalculateCenterOfMass(IList<Point> list)
+        {
+            return null;
+        }
+    }
+}
